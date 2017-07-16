32baecc411303c196ff9db7126c955ca966db75b
Peter Surowski <Peter Surowski>
2017-07-16 13:06:50 -0700

    Finally got the timer working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,15 @@
 // Copy for the intro screen saved in var.
-var introText = "> Clinging to the cliff's face, your party manages to find a way across the Gorge of Eternal Peril. It's the legendary Bridge of Death!";
-var continueText = "Click here to continue.";
-var questionsThree = "> STOP! Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.";
-var whatName = "> What is your name?";
+var introText = "Clinging to the cliff's face, your party manages to find a way across the Gorge of Eternal Peril. It's the legendary Bridge of Death!";
+var continueText = "> Click here to continue.";
+var notAfraid = "> Ask me the questions, bridge keeper. I'm not afraid!";
+var questionsThree = "STOP! Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.";
+var whatName = "What is your name?";
+var firstName = "> Sir Lancelot of Camelot.";
+var secondName = "> Sir Robin of Camelot.";
+var thirdName = "> Sir Galahad of Camelot.";
+var fourthName = "> It is Arthur, King of the Britons.";
+var whatQuest = "What is your quest?";
+
 
 //Declare a function that will trigger the click sound.
 function clickSound() {
@@ -21,6 +28,17 @@ function showText (target, message, index, interval) {
 		setTimeout(function() {
 			showText(target, message, index, interval);
 			clickSound();
+		}, interval);
+	}
+}
+
+// Declare a timer function.
+function timer (target, i, interval) {
+	if (i > -1) {
+		$(target).text(i--);
+		setTimeout(function() {
+			timer(target, i, interval);
+			console.log(i);
 		}, interval);
 	}
 }
@@ -51,14 +69,14 @@ $('#continue-text').click(function() {
 	$('#stop-screen').show();
 	// Text in .continue-text element is replaced with nothing.
 	$('.continue-text').text('');
-	//Slowly print the questionsThree variable to the #questionsThree element.
+	//Slowly print the questionsThree variable to the #questions-three element.
 	$(function () {
-		showText('#questionsThree', questionsThree, 0, 50);
+		showText('#questions-three', questionsThree, 0, 50);
 	});
 	//Then we wait six seconds and print to the continue-text element.		
 	setTimeout(function() {
 		$(function () {
-		showText('.continue-text', continueText, 0, 50)
+		showText('.continue-text', notAfraid, 0, 50)
 		});
 	}, 6000)
 });
@@ -68,9 +86,50 @@ $('#continue-text-two').click(function() {
 	//Stop screen is display: none-ed...
 	$('#stop-screen').hide();
 	//Text in .continue-text element is replaced with nothing...
-	$('.continue-text').text();
+	$('.continue-text').text('');
 	//And name-screen is display: block-ed.
 	$('#name-screen').show();
-
+	//Slowly print the what-name variable to the #whatName element.
+	$(function () {
+		showText('#what-name', whatName, 0, 50);
+	});
+	//Then we wait two seconds and print to the choices.		
+	setTimeout(function() {
+		$(function () {
+			showText('#lancelot', firstName, 0, 50)
+		});
+	}, 2000)
+	setTimeout(function() {
+		$(function() {
+			showText('#robin', secondName, 0, 50)
+		});
+	}, 3500)
+	setTimeout(function() {
+		$(function() {
+			showText('#galahad', thirdName, 0, 50)
+		});
+	}, 5000)
+	setTimeout(function() {
+		$(function() {
+			showText('#arthur', fourthName, 0, 50)
+		});
+	}, 6500)
+	//Wait eight seconds, then show the timer...
+	setTimeout(function() {
+		$('#timer').show();
+		//And call the timer function...
+		timer ('#timer', 10, 1000);
+	}, 8000)
 });
 
+//If the user clicks lancelot...
+$('#lancelot').click(function() {
+	//Hide name-screen...
+	$('#name-screen').hide();
+	//Show the lancelot-quest screen
+	$('#lancelot-screen').show();
+	//Print the whatQuest var to the what-quest element.
+	$(function () {
+		showText('#what-quest', whatQuest, 0, 50);
+	});
+})
